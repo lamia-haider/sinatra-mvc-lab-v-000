576d88ed98bdee237e6958e5818f74e700b93ef4
Lamia Haider <lamia.haider@gmail.com>
2020-12-16 18:12:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,7 @@ class PigLatinizer < Sinatra::Base
   def words(string)
     words = string.split(" ")
     words.map do |w|
-      if w.start_with?("a, e, i, o, u")
+      if w.start_with?("a", "e", "i", "o", "u")
         "#{w}way"
       end
     end
