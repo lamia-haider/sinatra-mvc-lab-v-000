1b784463eaba62530880725dfd8dc64d8fdfa3be
Lamia Haider <lamia.haider@gmail.com>
2020-12-16 19:42:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,7 @@ class PigLatinizer
   def piglatinize(user_input)
     words = user_input.split(" ")
     latinarr = words.collect do |w|
-      if w.start_with?([aeiou])
+      if w =~ /\A([aeiou])/
         "#{w}way"
       else arr = w.split(/([aeiou].*)/)
         "#{arr[1]}#{arr[0]}ay"
