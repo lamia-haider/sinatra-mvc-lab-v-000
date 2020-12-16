dc8981a246c05463b36d4edd57169b5b5113a55c
Lamia Haider <lamia.haider@gmail.com>
2020-12-16 19:37:35 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,7 @@ class PigLatinizer
   def piglatinize(user_input)
     words = user_input.split(" ")
     latinarr = words.collect do |w|
-      if w.start_with?("a", "e", "i", "o", "u")
+      if w.start_with?("a", "e", "i", "o", "u") || w == "A" || w =="I"
         "#{w}way"
       else arr = w.split(/([aeiou].*)/)
         "#{arr[1]}#{arr[0]}ay"
